595f40f249e14419261bebc8d6b2fb239ac0a308
Kane Li <122249106+Li-Kane@users.noreply.github.com>
2026-01-07 16:14:27 -0800

    Just old code
    Save all my old code without keeping it in the current codebase

--- frontend/src/types/other.ts
+++
@@ -1,9 +0,0 @@
-import { UserRecord } from "./db";
-
-export interface AuthResponse {
-  user: UserRecord;
-  token: string;
-}
-
-// equipment item background colors should be Hex
-export type Hex = `#${string}`;
